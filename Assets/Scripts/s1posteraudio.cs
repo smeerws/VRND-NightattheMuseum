c54c071eeaf88d7ed94b306d51c1710d232284af
smeerws <hello@smeerws.at>
2017-09-26 20:10:57 +0200

    added sound westworld and surrogates trailer mp3 to wp5 and wp4

---
+++ Assets/Scripts/s1posteraudio.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class s1posteraudio : MonoBehaviour {
+	public AudioClip startClip;
+	public AudioClip nextClip;
+	private AudioSource audio;
+	private bool isrunning = false;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+		audio.loop = true;
+	}
+
+	void Update(){
+		Debug.Log ("update poster" + WaypointMovement.mutePostersound);
+		if (WaypointMovement.mutePostersound && !isrunning) {
+			StartCoroutine (playNextSound ());
+			isrunning = true;
+		} 
+		if (!WaypointMovement.mutePostersound) {
+			isrunning = false;
+			audio.Pause ();
+		}
+	}
+
+	IEnumerator playNextSound(){
+		    Debug.Log ("playsound " + WaypointMovement.mutePostersound);
+			audio.clip = startClip;
+			audio.Play ();
+			yield return new WaitForSeconds (audio.clip.length);
+			audio.clip = nextClip;
+			audio.Play ();
+		}
+
+}
