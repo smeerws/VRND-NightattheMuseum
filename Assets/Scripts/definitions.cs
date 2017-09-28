287d2749ae8aa9cd64cf3f961e40e48024aa2f64
smeerws <hello@smeerws.at>
2017-09-28 14:37:09 +0200

    Station definitions added

--- Assets/Scripts/definitions.cs
+++ Assets/Scripts/definitions.cs
@@ -1,30 +1,89 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class definitions : MonoBehaviour {
 	private GameObject robot;
-	private bool isrunning = false;
+	private bool isdefactive;
+	private GameObject btnVR, btnRobot, btnTele, btnAR, btnAI;
+	public  GameObject vrplane, robotplane, teleplane, arplane, aiplane;
+	private GameObject active;
+
 	// Use this for initialization
 	void Start () {
 		robot = GameObject.Find ("Robot Kyle");
 		robot.GetComponent<Animation> ().enabled = false;
-		Debug.Log (robot.name);
+
+		btnRobot = GameObject.Find ("btnRobot");
+		btnTele = GameObject.Find ("btnTele");
+		btnVR = GameObject.Find ("btnVR");
+		btnAR = GameObject.Find ("btnAR");
+		btnAI = GameObject.Find ("btnAI");
+
+		vrplane.SetActive(false);
+		robotplane.SetActive (false);
+		teleplane.SetActive (false);
+		arplane.SetActive (false);
+		aiplane.SetActive (false);
+
+		isdefactive = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (WaypointMovement.humanoidrobot && !isrunning) {
-			Debug.Log ("W10");
-			isrunning = true;
-			robot.GetComponent<Animation> ().enabled = true;
-		} 
 
-		if (!WaypointMovement.humanoidrobot) {
-			Debug.Log ("!W10");
-			isrunning = false;
-			robot.GetComponent<Animation> ().enabled = false;
+		if (isdefactive) {
+			if (!active.GetComponent<AudioSource> ().isPlaying) {
+				active.gameObject.SetActive (false);
+				robot.GetComponent<Animation> ().enabled = false;
+				setButtonsActive ();
+			}
 		}
 		
+	}
+
+	private void startDefinition(GameObject current){
+		active = current;
+		active.gameObject.SetActive (true);
+		isdefactive = true;
+		robot.GetComponent<Animation> ().enabled = true;
+		setButtonsInactive ();
+	}
+
+	public void startVR(){
+		startDefinition (vrplane);
+	}
+
+	public void startAR(){
+		startDefinition(arplane);
+		}
+
+	public void startTele(){
+		startDefinition (teleplane);
+		}
+
+	public void startRobot(){
+		startDefinition (robotplane);
+		}
+
+	public void startAI(){
+		startDefinition (aiplane);
+	}
+
+	private void setButtonsInactive(){
+		btnRobot.GetComponent<Button> ().interactable = false;
+		btnTele.GetComponent<Button> ().interactable  = false;
+		btnVR.GetComponent<Button> ().interactable    = false;
+		btnAR.GetComponent<Button> ().interactable    = false;
+		btnAI.GetComponent<Button> ().interactable    = false;
+	}
+
+	private void setButtonsActive(){
+		btnRobot.GetComponent<Button> ().interactable = true;
+		btnTele.GetComponent<Button> ().interactable  = true;
+		btnVR.GetComponent<Button> ().interactable    = true;
+		btnAR.GetComponent<Button> ().interactable    = true;
+		btnAI.GetComponent<Button> ().interactable    = true;
 	}
 }
