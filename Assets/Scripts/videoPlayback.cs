d36c59896ecaffb751622c323159d4fd66525003
smeerws <hello@smeerws.at>
2017-10-07 17:16:53 +0200

    added flying drone station, model, animation, sound, video (there is an error reading encoded movie

---
+++ Assets/Scripts/videoPlayback.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class videoPlayback : MonoBehaviour {
+	private string movie = "drone-fade.mp4";
+	public int sceneNumber; 
+
+	// Use this for initialization
+	void Start () {
+		//StartCoroutine (streamVideo(movie));
+	}
+	
+	private IEnumerator streamVideo(string video){
+		Handheld.PlayFullScreenMovie (video, Color.black, FullScreenMovieControlMode.CancelOnInput, FullScreenMovieScalingMode.AspectFill);
+		yield return new WaitForEndOfFrame ();
+		Debug.Log ("Video playback is now completed");
+		//SceneManager.LoadScene (sceneNumber);
+	}
+
+	public void startVideo(){
+		StartCoroutine (streamVideo(movie));
+		Debug.Log ("Coroutine started");
+	}
+}
