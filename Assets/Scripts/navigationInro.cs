3cdbd14e5d48c90e3dfd4ad71d64a3219a369ec9
smeerws <hello@smeerws.at>
2017-10-13 11:27:26 +0200

    Navigation sprite added due to the comment of the reviewer, that this is missing.

---
+++ Assets/Scripts/navigationInro.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class navigationInro : MonoBehaviour {
+
+	private GameObject navSign;
+	private GameObject wp1, wp2, wp3;
+
+	// Use this for initialization
+	void Start () {
+		navSign = GameObject.Find ("navigation");
+
+		wp1 = GameObject.Find ("Waypoint_w1");
+		wp2 = GameObject.Find ("Waypoint_w2");
+		wp3 = GameObject.Find ("Waypoint_w3");
+
+		wp1.SetActive (false);
+		wp2.SetActive (false);
+		wp3.SetActive (false);
+
+	}
+
+	public void startExperience(){
+		wp1.SetActive (true);
+		wp2.SetActive (true);
+		wp3.SetActive (true);
+		navSign.SetActive (false);
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
